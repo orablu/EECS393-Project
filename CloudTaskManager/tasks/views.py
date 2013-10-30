80c6d855468e798febd2d0990a0e99367e462ad3
Wes Rupert <ora@outlook.com>
2013-10-29 23:11:33 -0400

    Update urls.py, views.py.

--- CloudTaskManager/tasks/views.py
+++ CloudTaskManager/tasks/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
-from tasks.models import TaskList
+from tasks.models import TaskList, Task
 
 
 def index(request):
@@ -13,10 +13,28 @@ def details(request, list_id):
     return render(request, 'tasks/details.html', context)
 
 
-def edit(request, list_id):
+def edit_list(request, list_id):
     context = {'tasklist': get_object_or_404(TaskList, pk=list_id)}
-    return render(request, 'tasks/edit.html', context)
+    return render(request, 'tasks/edit_list.html', context)
 
 
-def save(request, list_id):
+def edit_task(request, task_id):
+    context = {'task': get_object_or_404(Task, pk=task_id)}
+    return render(request, 'tasks/edit_task.html', context)
+
+
+def new_task(request, list_id):
+    context = {'list_id': list_id}
+    return render(request, 'tasks/new_task.html', context)
+
+
+def save_list(request):
+    return HttpResponse("Saved! (not actually though)")
+
+
+def save_task(request):
+    return HttpResponse("Saved! (not actually though)")
+
+
+def save_new_task(request):
     return HttpResponse("Saved! (not actually though)")
