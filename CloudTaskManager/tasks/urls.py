68f6f9c91080aa82f05230d2e9981482a4f83bed
chas11man <chas11man@gmail.com>
2013-10-29 22:50:00 -0400

    details working correctly

--- CloudTaskManager/tasks/urls.py
+++ CloudTaskManager/tasks/urls.py
@@ -4,7 +4,7 @@ from tasks import views
 urlpatterns = patterns('',
         url(r'^$', views.index, name='index'), # e.g. /tasklists/
         url(r'^(?P<list_id>\d+)/$', views.details, name='details'), # e.g. /tasklists/0/
-        url(r'^(?P<list_id>\d+)/edit/$', views.edit, name='details'), # e.g. /tasklists/0/edit/
+        url(r'^(?P<list_id>\d+)/edit/$', views.edit, name='edit'), # e.g. /tasklists/0/edit/
         url(r'^(?P<list_id>\d+)/save/$', views.save, name='save'),
 )
 
--- CloudTaskManager/tasks/views.py
+++ CloudTaskManager/tasks/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, Http404
 from django.shortcuts import render, get_object_or_404
-from tasks.models import TaskList
+from tasks.models import TaskList, Task
+from tasks.forms import TaskForm
 
 def index(request):
     task_list_list = TaskList.objects.order_by('title')
@@ -8,12 +9,21 @@ def index(request):
     return render(request, 'tasks/index.html', context)
 
 def details(request, list_id):
-    context = {'tasklist': get_object_or_404(TaskList, pk=list_id)}
-    return render(request, 'tasks/details.html', context)
+	try:
+		tasklist = TaskList.objects.get(pk=list_id)
+		tasks_list = tasklist.task_set.all()
+		context = {'tasklist': tasklist, 'tasks_list': tasks_list}
+	except:
+		raise Http404
+	return render(request, 'tasks/details.html', context)
 
 def edit(request, list_id):
-    context = {'tasklist': get_object_or_404(TaskList, pk=list_id)}
-    return render(request, 'tasks/edit.html', context)
+	if request.method == 'POST':
+		form = TaskForm(request.POST)
+		if form.is_valid():
+			title = form.cleaned_data['title']
+			description = form.cleaned_data['description']
+			form.save(title, description)
 
 def save(request, list_id):
     return HttpResponse("Saved! (not actually though)")
