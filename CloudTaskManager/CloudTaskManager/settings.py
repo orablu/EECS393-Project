8af0f69b8e4f1dbff110a3376b0a47aa8b11eedb
Shelley Murphy <sam221@case.edu>
2013-10-30 12:04:46 -0400

    Changed where login redirects to

--- CloudTaskManager/CloudTaskManager/settings.py
+++ CloudTaskManager/CloudTaskManager/settings.py
@@ -66,7 +66,7 @@ STATIC_ROOT = ''
 # Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL = '/static/'
 
-LOGIN_REDIRECT_URL = '/tasklists/1/'
+LOGIN_REDIRECT_URL = '/tasklists/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
