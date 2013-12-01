9bafce86dee176a2d1f9203fd833161a350166e6
Wes Rupert <ora@outlook.com>
2013-12-01 01:18:16 -0500

    Style changes.

--- CloudTaskManager/tasks/models.py
+++ CloudTaskManager/tasks/models.py
@@ -3,7 +3,7 @@ import datetime
 from django.utils import timezone
 from django.contrib.auth.models import User as AuthUser
 
-TITLE_LENGTH = 50
+TITLE_LENGTH = 25
 DESCR_LENGTH = 300
 CATEG_LENGTH = 20
 LATE_STR = 'late'
