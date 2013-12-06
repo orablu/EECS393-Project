2cde6401b2f7b53282ea567ee81b88924c40d36f
sam221 <sam221@case.edu>
2013-12-05 23:27:52 -0500

    Update tests.py
    Full set of tests, 100% coverage on all but views.py

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,7 +1,12 @@
 from django.test import TestCase
-from tasks.models import TaskList, Task
+from tasks.models import TaskList, Task, User
+from django.contrib.auth.models import User as AuthUser
+import tasks.forms
+import tasks.urls
+import tasks.views
 from django.utils import timezone
 import datetime
+from django.contrib.auth import authenticate, login
 
 
 class TaskListMethodTests(TestCase):
@@ -12,6 +17,37 @@ class TaskListMethodTests(TestCase):
     TASK_DESCR = 'TEST_DESCR'
     TASK_CATEGORY = 'TEST_TASKS'
     TASK_DUEDATE = datetime.datetime(2003, 8, 4, 7, 17)
+
+    AuthUser.objects.create_user('test',
+                                 'test@test.com',
+                                 'test')
+    authuser = authenticate(username='test',
+                            password='test')
+    def test_string_function(self):
+        tasklist = TaskList(title=self.TASKLIST_TITLE,
+                            description=self.TASKLIST_DESCR,
+                            category=self.TASKLIST_CATEG)
+        self.assertEqual(tasklist.__str__(), 'TEST___TESTTASKLIST: A TEST TASK LIST')
+
+    def test_string_function_task(self):
+        tasklist = TaskList(title=self.TASKLIST_TITLE,
+                            description=self.TASKLIST_DESCR,
+                            category=self.TASKLIST_CATEG)
+        task = Task(tasklist=tasklist,
+                    title=self.TASK_TITLE,
+                    description=self.TASK_DESCR,
+                    category=self.TASK_CATEGORY,
+                    due_date=self.TASK_DUEDATE,
+                    is_completed=False)
+        self.assertEqual(task.__str__(), 'TEST_TASK: TEST_DESCR')
+
+    def test_get_username_and_names(self):
+        authuser = authenticate(username='test',
+                                password='test')
+        user = User(authuser=authuser)
+        self.assertEqual(user.get_username(), "test")
+        self.assertEqual(user.get_firstname(), "")
+        self.assertEqual(user.get_name(), "")
 
     def test_create_tasklist(self):
         """
@@ -198,3 +234,142 @@ class TaskListMethodTests(TestCase):
                     due_date=new_due_date,
                     is_completed=False)
         self.assertEqual(task.status(), 'due tomorrow')
+
+    def test_user_can_write_owned(self):
+        tasklist = TaskList(title=self.TASKLIST_TITLE,
+                            description=self.TASKLIST_DESCR,
+                            category=self.TASKLIST_CATEG)
+        authuser = authenticate(username='test',
+                                password='test')
+        user = User(authuser=authuser)
+        tasklist.save()
+        user.save()
+        user.owned.add(tasklist)
+        self.assertTrue(tasks.views.user_can_write(user,tasklist))
+
+    def test_user_can_write_shared(self):
+        tasklist = TaskList(title=self.TASKLIST_TITLE,
+                            description=self.TASKLIST_DESCR,
+                            category=self.TASKLIST_CATEG)
+        authuser = authenticate(username='test',
+                                password='test')
+        user = User(authuser=authuser)
+        tasklist.save()
+        user.save()
+        authuser.user.shared.add(tasklist)
+        self.assertTrue(tasks.views.user_can_write(user,tasklist))
+
+    def test_user_cannot_write_readonly(self):
+        tasklist = TaskList(title=self.TASKLIST_TITLE,
+                            description=self.TASKLIST_DESCR,
+                            category=self.TASKLIST_CATEG)
+        authuser = authenticate(username='test',
+                                password='test')
+        user = User(authuser=authuser)
+        tasklist.save()
+        user.save()
+        authuser.user.readonly.add(tasklist)
+        self.assertFalse(tasks.views.user_can_write(user,tasklist))
+
+    def test_users_should_be_logged_in(self):
+        request = self.client.get('/login/', follow=True)
+        self.assertTemplateUsed(request, 'tasks/login.html')
+        self.assertEqual(request.status_code, 200)
+        request = self.client.post('/url/to/view', follow=True)
+        self.assertEqual(request.status_code, 404)
+
+    def test_user_login_again(self):
+        request = self.client.get('/register/', follow=True)
+        self.assertEqual(request.status_code, 200)
+        self.assertTemplateUsed(request, 'tasks/register.html')
+        response = self.client.post('/register/', follow=True)
+        self.assertTemplateUsed(response, 'tasks/register.html')
+        self.assertEqual(response.status_code, 200)
+       
+    def something(self):
+        self.client.login(username='user', password='test')  # defined in fixture
+        response = self.client.get('/index/', follow=True)
+        self.assertRedirects(request, '/login/')
+
+    def test_add_list(self):
+        self.client.login(username='test', password='test')
+        response = self.client.get('/add/', follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed('tasks/add.html')
+
+    def test_details(self):
+        self.client.login(username='test', password='test')
+        tasklist = TaskList(title=self.TASKLIST_TITLE,
+                            description=self.TASKLIST_DESCR,
+                            category=self.TASKLIST_CATEG)
+        authuser = authenticate(username='test',
+                                password='test')
+        user = User(authuser=authuser)
+        tasklist.save()
+        user.save()
+        user.owned.add(tasklist)
+        response = self.client.get('/1/', follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed('tasks/details.html')
+        
+    def test_cannot_edit_list(self):
+        self.client.login(username='test', password='test')
+        tasklist = TaskList(title=self.TASKLIST_TITLE,
+                            description=self.TASKLIST_DESCR,
+                            category=self.TASKLIST_CATEG)
+        authuser = authenticate(username='test',
+                                password='test')
+        user = User(authuser=authuser)
+        tasklist.save()
+        user.save()
+        user.readonly.add(tasklist)
+        response = self.client.get('/1/edit/', follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed('tasks/edit.html')
+
+    def test_can_edit_list(self):
+        self.client.login(username='test', password='test')
+        tasklist = TaskList(title=self.TASKLIST_TITLE,
+                            description=self.TASKLIST_DESCR,
+                            category=self.TASKLIST_CATEG)
+        authuser = authenticate(username='test',
+                                password='test')
+        user = User(authuser=authuser)
+        tasklist.save()
+        user.save()
+        user.shared.add(tasklist)
+        response = self.client.get('/1/edit/', follow=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_shared_lists(self): 
+        self.client.login(username='test', password='test')
+        tasklist = TaskList(title=self.TASKLIST_TITLE,
+                            description=self.TASKLIST_DESCR,
+                            category=self.TASKLIST_CATEG)
+        authuser = authenticate(username='test',
+                                password='test')
+        user = User(authuser=authuser)
+        tasklist.save()
+        user.save()
+        user.owned.add(tasklist)
+        response = self.client.get('/1/share/', follow=True)
+        request = self.client.post('/1/share/', follow=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_add_tasklist(self):
+        self.client.login(username='test', password='test')
+        response = self.client.get('/add/', follow=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_add_task(self):
+        self.client.login(username='test', password='test')
+        tasklist = TaskList(title=self.TASKLIST_TITLE,
+                            description=self.TASKLIST_DESCR,
+                            category=self.TASKLIST_CATEG)
+        authuser = authenticate(username='test',
+                                password='test')
+        user = User(authuser=authuser)
+        tasklist.save()
+        user.save()
+        response = self.client.get('/1/add/', follow=True)
+        self.assertTemplateUsed("tasks/add.html") 
